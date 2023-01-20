248ee96e00a88a0ff3316b58c8e5c622ce043808
Taslim <taslimahmed313@gmil.com>
2023-01-20 15:06:23 +0600

    All Menu Added

---
+++ src/components/FoodList/FoodCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./FoodCard.css";
+
+const FoodCard = ({item}) => {
+    const {img, price, name, food_element} = item;
+    return (
+        <div className='food_card hover_effect'>
+            <img src={img} alt="food_card" />
+            <div className='food_card_info'>
+                <h1 className='food_card_heading '>{name}</h1>
+                <p className='food_card_food_element'>{food_element}</p>
+                <p className='food_card_food_price'><span>BDT</span> {price}++</p>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/components/FoodList/FoodList.jsx
+++ src/components/FoodList/FoodList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import "./FoodList.css";
 
-import { images } from '../../constants';
+import SubHeading from '../SubHeading/SubHeading';
+import FoodCard from './FoodCard';
 
 
 const FoodList = () => {
@@ -11,19 +13,29 @@ const FoodList = () => {
         .then(res => res.json())
         .then(data => setStarters(data))
     },[])
-    console.log(setStarters);
     return (
-        <div className='app__bg'>
-            <h1 className='headtext__cormorant'>Menu</h1>
-            <div>
-                {
+        <div className='app__bg section__padding'>
+             <div className='food_menu'>
+                <h1 className='headtext__cormorant'>Our Menu</h1>
+                <SubHeading title={"The most delicious dishes made from the freshest of ingredients"}/>
+            </div>
+            <div className='items_grid'>
+                {/* {
                 starters.map(food => <div>
-                    <div>
-                        {food.status === "available" && <img src={images.check_mark} 
-                        alt='check_mark'/>}
-                        {food.name}
+                    <div className='food_list_flex'>
+                        {food.status === "available" ? <img className='img_check_mark' src={images.check_mark} 
+                        alt='check_mark'/> : <p className='margin_left'></p>}
+                        {food.img && <img className='food_img' src={food.img} alt="" />}
+                        <div>
+                            <h2>{food.name}</h2>
+                            <p>{food.food_element}</p>
+                        </div>
+                        <p className='food_price'><span>BDT</span> {food.price}</p>
                     </div>
                 </div>)
+            } */}
+            {
+                starters.map(item => <FoodCard item={item}/>)
             }
             </div>
         </div>
