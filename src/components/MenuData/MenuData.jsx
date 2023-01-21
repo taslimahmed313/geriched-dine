88d98601cd86c5617695e83dddcaab0f82e52751
Taslim <taslimahmed313@gmil.com>
2023-01-21 22:41:12 +0600

    Ready Soup and Salad Data

--- src/components/MenuData/MenuData.jsx
+++ src/components/MenuData/MenuData.jsx
@@ -62,3 +62,69 @@
         "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Arancini-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Hummus-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Gyro-Platter-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Prawn-Croquettes-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Harissa-Mushroom-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Crostini-Pizzaiola-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Mezze-Platter-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Mozzarella-Sticks-1-1-pnvmgmdnk67gc80njzmuuusuqo18qy1olsg0nln2i8.jpg"
     ]
 
+
+
+// Soups and Salad Data--------------------------------------------------->>>>>>
+    export const salad = [
+        {
+            id: "1",
+            name: "Caesar Salad",
+            img: "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Caesar-Salad-1-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg",
+            price: "750",
+            food_element: "Grilled Chicken Thigh, Seasonal Greens, Tomatoes, Cucumber, Beef Bacon, Boiled Egg."
+        },
+        {
+            id: "2",
+            name: "Roasted Beetroot Salad with Feta",
+            img: "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Roasted-Beetroot-Salad-with-Feta-1-pnvmgo9bxua0zfxx90g3zubrxfrz6c95a1qzm5ka5s.jpg",
+            price: "700",
+            food_element: "Beetroot, Seasonal Greens, Cucumber, Tomatoes, Feta, Couscous, Harissa Dressing"
+        },
+        {
+            id: "3",
+            name: "Snapper Niçoise",
+            img: "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Snapper-Nicoise-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg",
+            price: "1150",
+            food_element: "Grilled Red Snapper, Boiled Egg, Seasonal Greens, Cucumber, Tomatoes"
+        },
+        {
+            id: "4",
+            name: "Roasted Cauliflower Soup",
+            img: "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Roasted-Cauliflower-1-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg",
+            price: "500",
+            food_element: "Cauliflower, Cream, Vegetable Broth, Cheddar"
+        },
+        {
+            id: "5",
+            name: "French Onion Soup",
+            img: "https://thumbs.dreamstime.com/b/french-onion-soup-see-my-other-works-portfolio-36811119.jpg",
+            price: "500",
+            food_element: "Caramelized Red & White Onions, Cheese Crouton, Beef Broth"
+        },
+        {
+            id: "6",
+            name: "Couscous Salad with Feta",
+            img: "https://media.istockphoto.com/id/924744284/photo/tabbouleh-salad-with-tomato-cucumber-couscous-mint-and-pomegranate-vegan-healthy-food-concept.jpg?s=612x612&w=0&k=20&c=tGDaGmhegZyBi5rK-v1TAXByQL0RSlZeYPneQ_Xf_bE=",
+            price: "650",
+            food_element: "Feta, Seasonal Greens, Cucumber, Tomatoes, Couscous, Harissa Dressing"
+        },
+        {
+            id: "7",
+            name: "Couscous Salad with Chicken",
+            img: "https://media.istockphoto.com/id/653919416/photo/couscous-with-haehnchenbruts.jpg?s=612x612&w=0&k=20&c=AgdLbRBlbptMPu_XwsbivnjQoGGHWfWfY2H0cq73JTE=",
+            price: "750",
+            food_element: "Grilled Chicken Thigh, Seasonal Greens, Cucumber, Tomatoes, Couscous, Harissa Dressing"
+        },
+        {
+            id: "7",
+            name: "Cream of Mushroom Soup",
+            img: "https://media.istockphoto.com/id/597926666/photo/mushroom-soup-with-parsley.jpg?s=612x612&w=0&k=20&c=xhUB_u_bUQoumN-uzJQHCMglPlGWJWcnH32lcU-mJkg=",
+            price: "500",
+            food_element: "Sautéed Button Mushrooms, Smoked Chicken Cubes, Chicken Broth"
+        }
+    ]
+
+    // Soups and Salads Slider Images--------------------------->>>>>>>>>>>>>>
+    export const salad_img = [
+        "https://bistro-e.com/wp-content/uploads/elementor/thumbs/Caesar-Salad-1-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Roasted-Beetroot-Salad-with-Feta-1-pnvmgo9bxua0zfxx90g3zubrxfrz6c95a1qzm5ka5s.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Snapper-Nicoise-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg","https://bistro-e.com/wp-content/uploads/elementor/thumbs/Roasted-Cauliflower-1-1-pnvmgnbhr08qntzaei1hfckbc1wlyn5exx3i4vloc0.jpg","https://thumbs.dreamstime.com/b/french-onion-soup-see-my-other-works-portfolio-36811119.jpg","https://media.istockphoto.com/id/924744284/photo/tabbouleh-salad-with-tomato-cucumber-couscous-mint-and-pomegranate-vegan-healthy-food-concept.jpg?s=612x612&w=0&k=20&c=tGDaGmhegZyBi5rK-v1TAXByQL0RSlZeYPneQ_Xf_bE=","https://media.istockphoto.com/id/653919416/photo/couscous-with-haehnchenbruts.jpg?s=612x612&w=0&k=20&c=AgdLbRBlbptMPu_XwsbivnjQoGGHWfWfY2H0cq73JTE=","https://media.istockphoto.com/id/597926666/photo/mushroom-soup-with-parsley.jpg?s=612x612&w=0&k=20&c=xhUB_u_bUQoumN-uzJQHCMglPlGWJWcnH32lcU-mJkg="
+    ]
